a0ed0bd605cd25e734b86c471acbb9cbf129441c
Москвин Ангархаев <zn-mosk@mail.ru>
2024-06-12 17:35:25 +0300

    9.6. work. add urls

---
+++ mysite/myauth/urls.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.views import LoginView
+from django.urls import path
+from .views import MyLogoutView, set_cookies_view, get_cookies_view, set_session_view, get_session_view
+
+app_name = 'myauth'
+
+urlpatterns = [
+    path('login/', LoginView.as_view(
+        template_name='myauth/login.html',
+        redirect_authenticated_user=True
+    ),
+        name='login',
+        ),
+    path('logout/', MyLogoutView.as_view(), name='logout'),
+    path('set_cookie/', set_cookies_view, name='set_cookie'),
+    path('get_cookie/', get_cookies_view, name='get_cookie'),
+    path('set_session/', set_session_view, name='set_session'),
+    path('get_session/', get_session_view, name='get_session'),
+]
+
